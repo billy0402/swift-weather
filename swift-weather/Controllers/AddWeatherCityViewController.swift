48485cac364e8a516c45ce1f51677b3c87d9d419
Billy Huang <10646003@ntub.edu.tw>
2020-03-16 00:50:23 +0800

    Testing the Webservice Class

--- swift-weather/Controllers/AddWeatherCityViewController.swift
+++ swift-weather/Controllers/AddWeatherCityViewController.swift
@@ -14,7 +14,17 @@ class AddWeatherCityViewController: UIViewController {
     @IBOutlet weak var cityNameTextField: UITextField!
     
     @IBAction func saveCityButtonPressed() {
-        
+        if let city = cityNameTextField.text {
+            let weatherURL = URL(string: "https://api.openweathermap.org/data/2.5/weather?q=\(city)&appid=7d2dd8c9c5578b741c7735ad3f0d39ea&units=imperial")!
+            
+            let weatherResource = Resource<Any>(url: weatherURL) { data in
+                return data
+            }
+            
+            WebService().load(resource: weatherResource) { result in
+                
+            }
+        }
     }
     
     @IBAction func close() {
--- swift-weather/Services/WebService.swift
+++ swift-weather/Services/WebService.swift
@@ -17,6 +17,8 @@ final class WebService {
 
     func load<T>(resource: Resource<T>, completion: @escaping (T?) -> ()) {
         URLSession.shared.dataTask(with: resource.url) { data, response, error in
+            print(data)
+            
             if let data = data {
                 DispatchQueue.main.async {
                     completion(resource.parse(data))
