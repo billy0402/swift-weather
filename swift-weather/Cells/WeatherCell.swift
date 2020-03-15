9e5ce630038c752694841e2bfc9af30b83f9a45f
Billy Huang <10646003@ntub.edu.tw>
2020-03-15 10:58:51 +0800

    Displaying Hard-Coded Weather into TableView

---
+++ swift-weather/Cells/WeatherCell.swift
@@ -0,0 +1,15 @@
+//
+//  WeatherCell.swift
+//  swift-weather
+//
+//  Created by User on 2020/3/15.
+//  Copyright © 2020 NTUB. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class WeatherCell: UITableViewCell {
+    @IBOutlet weak var cityNameLabel: UILabel!
+    @IBOutlet weak var temperatureNameLabel: UILabel!
+}
