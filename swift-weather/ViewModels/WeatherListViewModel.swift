bc0639c999ec905be74bb6e3be68d4143a9fd949
Billy Huang <10646003@ntub.edu.tw>
2020-03-16 00:48:48 +0800

    Implementing View Models

---
+++ swift-weather/ViewModels/WeatherListViewModel.swift
@@ -0,0 +1,13 @@
+//
+//  WeatherListViewModel.swift
+//  swift-weather
+//
+//  Created by User on 2020/3/16.
+//  Copyright © 2020 NTUB. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherListViewModel {
+    private var weatherViewModels = [WeatherViewModel]()
+}
---
+++ swift-weather/ViewModels/WeatherViewModel.swift
@@ -0,0 +1,16 @@
+//
+//  WeatherViewModel.swift
+//  swift-weather
+//
+//  Created by User on 2020/3/16.
+//  Copyright © 2020 NTUB. All rights reserved.
+//
+
+import Foundation
+
+struct WeatherViewModel {
+    let name: String
+    let temperature: Double
+    let temperatureMin: Double
+    let temperatureMax: Double
+}
