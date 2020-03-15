5ae00a2c2360e4cb76e278e01b26f26efaeab2fc
Billy Huang <10646003@ntub.edu.tw>
2020-03-15 11:07:58 +0800

    Designing the AddWeather Screen

--- swift-weather/Controllers/AddWeatherCityViewController.swift
+++ swift-weather/Controllers/AddWeatherCityViewController.swift
@@ -10,5 +10,15 @@ import Foundation
 import UIKit
 
 class AddWeatherCityViewController: UIViewController {
+
+    @IBOutlet weak var cityNameTextField: UITextField!
     
+    @IBAction func saveCityButtonPressed() {
+        
+    }
+    
+    @IBAction func close() {
+        self.dismiss(animated: true, completion: nil)
+    }
+
 }
