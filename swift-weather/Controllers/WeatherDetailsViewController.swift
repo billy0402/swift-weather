28f9a28ec2f4075060d5071fb99ee845384a0e04
Billy Huang <10646003@ntub.edu.tw>
2020-03-20 21:03:58 +0800

    ViewModel to View - Binding the Values to User Interface

--- swift-weather/Controllers/WeatherDetailsViewController.swift
+++ swift-weather/Controllers/WeatherDetailsViewController.swift
@@ -21,9 +21,31 @@ class WeatherDetailsViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        if let weatherViewModel = self.weatherViewModel {
-            self.cityNameLabel.text = weatherViewModel.name.value
-            self.currentTemperatureLabel.text = weatherViewModel.currentTemperature.temperature.value.formatAsDegree
+        self.setupVMBindings()
+    }
+    
+    private func setupVMBindings() {
+        if let weatherVM = self.weatherViewModel {
+            weatherVM.name.bind {
+                self.cityNameLabel.text = $0
+            }
+            
+            weatherVM.currentTemperature.temperature.bind {
+                self.currentTemperatureLabel.text = $0.formatAsDegree
+            }
+            
+            weatherVM.currentTemperature.temperatureMin.bind {
+                self.minTemperatureLabel.text = $0.formatAsDegree
+            }
+            
+            weatherVM.currentTemperature.temperatureMax.bind {
+                self.maxTemperatureLabel.text = $0.formatAsDegree
+            }
+        }
+        
+        // change the value after few seconds
+        DispatchQueue.main.asyncAfter(deadline: .now() + 2.0) {
+            self.weatherViewModel?.name.value = "Boston"
         }
     }
 
